e0a0f929eecbc60b18b3069a6b36c2f9ed69edd8
mv1223 <mohanvamsi885@gmail.com>
2026-05-27 14:31:58 +0530

    Deploy: Update API URL and Cookie settings for production deployment

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -113,8 +113,8 @@ export const login = async (req, res) => {
         return res.status(200).cookie("token", token, { 
             maxAge: 1 * 24 * 60 * 60 * 1000, 
             httpOnly: true, 
-            sameSite: 'lax', 
-            secure: false 
+            sameSite: 'none', 
+            secure: true 
         }).json({
             message: `Welcome back ${user.fullname}`,
             user,
@@ -133,8 +133,8 @@ export const logout = async (req, res) => {
         return res.status(200).cookie("token", "", { 
             maxAge: 0,
             httpOnly: true,
-            sameSite: 'lax',
-            secure: false
+            sameSite: 'none',
+            secure: true
         }).json({
             message: "Logged out successfully.",
             success: true
